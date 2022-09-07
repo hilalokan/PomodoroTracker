0c49bfe497d235e182e3cb73df9af586fa1cd1de
hilalokan <91684373+hilalokan@users.noreply.github.com>
2022-09-07 16:04:47 +0300

    updated

--- PomodoroTracker/Controllers/HomeController.cs
+++ PomodoroTracker/Controllers/HomeController.cs
@@ -18,13 +18,6 @@ namespace PomodoroTracker.Controllers
             return View();
         }
 
-        public IActionResult Pomodoro()
-        {
-            return View();
-        }
-
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
